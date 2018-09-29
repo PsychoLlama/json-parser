7092118e978ff3206cdee1810add11418d5598d0
Jesse Gibson <JesseTheGibson@gmail.com>
2018-09-29 12:28:14 -0600

    Implement input stream
    Next up, the token stream!

---
+++ workspaces/json-parser/src/input-stream.js
@@ -0,0 +1,56 @@
+// @flow
+export default function InputStream(sourceText: string) {
+  let index = 0;
+  let column = 0;
+  let line = 0;
+
+  function createCodeFrame(line: number, column: number): string {
+    const lines = sourceText.split('\n');
+    const startingLine = Math.max(line - 2, 0);
+    const endingLine = Math.min(line + 2, lines.length);
+    const startingFrame = lines.slice(startingLine, line + 1);
+    const errorHighlight =
+      Array(Math.max(column - 1, 0))
+        .fill(' ')
+        .join('') + '^';
+
+    const endingFrame = lines.slice(line + 1, endingLine);
+
+    return (
+      '\n' +
+      startingFrame
+        .concat(errorHighlight)
+        .concat(endingFrame)
+        .map(line => '  ' + line)
+        .join('\n') +
+      '\n'
+    );
+  }
+
+  return new class InputStream {
+    consumeNextChar = (): string => {
+      if (this.peek() === '\n') {
+        column = 0;
+        line += 1;
+      } else {
+        column += 1;
+      }
+
+      index += 1;
+      return this.peek();
+    };
+
+    peek = (): string => {
+      return sourceText[index] || '';
+    };
+
+    eof = () => {
+      return !this.peek();
+    };
+
+    die = (message: string) => {
+      const frame = createCodeFrame(line, column);
+      throw new Error(message + '\n' + frame);
+    };
+  }();
+}
